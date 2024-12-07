35830553ae00deeed1dc8885479c12446c233089
Lil_ohmmy <tenda2526@gmail.com>
2024-12-07 16:10:04 +0700

    Delete Day4

--- Day4/target/debug/deps/Day4-a63cffbe2e308bf9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\msi-ohm\OneDrive\Desktop\adventofcode\Day4\target\debug\deps\libDay4-a63cffbe2e308bf9.rmeta: src/main.rs
-
-C:\Users\msi-ohm\OneDrive\Desktop\adventofcode\Day4\target\debug\deps\Day4-a63cffbe2e308bf9.d: src/main.rs
-
-src/main.rs:
--- Day4/target/debug/deps/Day4-e0b730413a11261a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\msi-ohm\OneDrive\Desktop\adventofcode\Day4\target\debug\deps\libDay4-e0b730413a11261a.rmeta: src/main.rs
-
-C:\Users\msi-ohm\OneDrive\Desktop\adventofcode\Day4\target\debug\deps\Day4-e0b730413a11261a.d: src/main.rs
-
-src/main.rs:
