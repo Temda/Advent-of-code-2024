55927905d7ba2b4d5170f874fcc90d7407f8f841
Lil_ohmmy <tenda2526@gmail.com>
2024-12-22 19:45:51 +0700

    update Day 4: Ceres Search

--- Day4/src/main.rs
+++ Day4/src/main.rs
@@ -1,17 +1,18 @@
-
-
 fn main() {
     let map = include_bytes!("../input.txt")
         .split(|&c| c == b'\n')
         .collect::<Vec<_>>();
 
-    part_1(&map);
-    part_2(&map);
+    let count1 = count_matches(&map, b"XMAS", b"SAMX");
+    println!("Part 1: The word 'XMAS' or 'SAMX' appears {} times.", count1);
+
+    let count2 = count_cross_pattern(&map);
+    println!("Part 2: The X-MAS pattern appears {} times.", count2);
 }
 
-fn part_1(map: &[&[u8]]) {
+fn count_matches(map: &[&[u8]], word1: &[u8], word2: &[u8]) -> usize {
     let mut word = [0; 4];
-    let count = (0..map[0].len() as isize)
+    (0..map[0].len() as isize)
         .flat_map(|x| (0..map.len() as isize).map(move |y| (x, y)))
         .flat_map(|(x, y)| {
             [
@@ -28,17 +29,14 @@ fn part_1(map: &[&[u8]]) {
                     .unwrap_or_default()
             });
             word.fill_with(|| iter.next().unwrap_or_default());
-            &word == b"XMAS" || &word == b"SAMX"
+            &word == word1 || &word == word2
         })
-        .count();
-
-    println!("Part 1: The word 'XMAS' or 'SAMX' appears {} times.", count);
+        .count()
 }
 
-fn part_2(map: &[&[u8]]) {
+fn count_cross_pattern(map: &[&[u8]]) -> usize {
     let mut cross = [0; 4];
-
-    let count = (0..map[0].len() as isize)
+    (0..map[0].len() as isize)
         .flat_map(|x| (0..map.len() as isize).map(move |y| (x, y)))
         .map(|(x, y)| {
             [
@@ -67,7 +65,5 @@ fn part_2(map: &[&[u8]]) {
 
             &cross == b"MMSS" || &cross == b"MSMS" || &cross == b"SSMM" || &cross == b"SMSM"
         })
-        .count();
-
-    println!("Part 2: The X-MAS pattern appears {} times.", count);
+        .count()
 }
